56bcae2b820cb7f83485bc2939cb11ac1e515ab5
shishirbhandari <shishirbhandari128@gmail.com>
2026-09-09 23:32:10 +0545

    feat: initialize Django settings and update docker-compose to use external env file for database configuration

--- digital_order_management_system/settings.py
+++ digital_order_management_system/settings.py
@@ -114,9 +114,9 @@ WSGI_APPLICATION = 'digital_order_management_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'digital_order_management_db'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'NAME': os.getenv('POSTGRES_DB', 'digital_order_management_db'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
